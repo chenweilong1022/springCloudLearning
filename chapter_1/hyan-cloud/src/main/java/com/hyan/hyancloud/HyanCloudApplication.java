74726424e9ca218bd399dcfbdc6060eaa3e399c2
chenweilong <chenweilong>
2019-01-10 22:55:19 +0800

    第一次提交

---
+++ chapter_1/hyan-cloud/src/main/java/com/hyan/hyancloud/HyanCloudApplication.java
@@ -0,0 +1,14 @@
+package com.hyan.hyancloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyanCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyanCloudApplication.class, args);
+    }
+
+}
+
